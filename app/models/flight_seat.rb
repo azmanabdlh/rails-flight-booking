39894cad2c240a046c5b2828267daa988316ffd8
Azman <macbookpro@MacBook-Pro-13.local>
2026-02-05 13:43:51 +0900

    add enum availability state

--- app/models/flight_seat.rb
+++ app/models/flight_seat.rb
@@ -1,3 +1,10 @@
 class FlightSeat < ApplicationRecord
   belongs_to :flight
+
+  enum :availability_state, {
+    locked: "LOCKED",
+    available: "AVAILABLE",
+    reserved: "RESERVED"
+  }
+
 end

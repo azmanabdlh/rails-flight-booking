d0158bb324d05cd55b9984536b47185c8b500db5
Azman <macbookpro@MacBook-Pro-13.local>
2026-02-06 01:03:16 +0900

    add ticket enum status

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,13 @@
 class Ticket < ApplicationRecord
   belongs_to :booking
   belongs_to :flight
+
+  enum :status, {
+    issued: 1,
+    boarding: 2,
+    departed: 3,
+    arrived: 4
+    # ....
+  }
+
 end

5acba9d7617295fe9e450289866cbbcb99a59278
Azman <macbookpro@MacBook-Pro-13.local>
2026-02-07 21:18:17 +0900

    add flight seat serializer

--- app/controllers/flight_seat_controller.rb
+++ app/controllers/flight_seat_controller.rb
@@ -1,39 +1,23 @@
 class FlightSeatController < ApplicationController
   def call
     begin
-      new_schema(
-        Flight.find(
-          params(:flight_id)
-        )
-      )
 
+      render json: { message: "ok",
+        data: serialize(
+          Flight.find(
+            params(:flight_id)
+          )
+        )
+      }, status: :ok
     rescue ActiveRecord::RecordNotFound
-      render json: { message: "Seat not found" }, status: :not_found
+      render json: { message: "Seat not found", data: [] }, status: :not_found
     rescue => e
-      render json: { message: e.message }, status: :bad_request
+      render json: { message: e.message, data: [] }, status: :bad_request
     end
   end
 
   private
-  def new_schema(flight)
-    Struct.new(
-      :flight_code,
-      :departure_airport,
-
-    )
-
-    # flight.cabins.map { |cabin|  new_cabin.build(cabin) }
-  end
-
-  def
-
-  def new_cabin
-    Struct.new(
-      :cabin_code,
-      :cabin_class,
-      :rows,
-      :seat_columns,
-      :seats
-    )
+  def serialize(flight)
+     FlightSeatSerializer.new(flight).serialize
   end
 end
---
+++ app/serializers/flight_seat_serializer.rb
@@ -0,0 +1,1 @@
+class FlightSeatSerializer; end

0486d7d940d950f9dc7c216b8b8ccdba51e20ab2
Azman <macbookpro@MacBook-Pro-13.local>
2026-02-09 08:28:43 +0900

    add component

--- app/serializers/flight_seat_serializer.rb
+++ app/serializers/flight_seat_serializer.rb
@@ -10,27 +10,38 @@ class FlightSeatSerializer
       :arrival_time
 
 
-    attribute :aircraft do |flight|
-      { model: flight.aircraft.model, variant: flight.aircraft.variant }
-    end
+      nested :aircraft do
+        attribute :information do |flight|
+          { model: flight.aircraft.model, variant: flight.aircraft.variant }
+        end
+
+        attribute :available_cabins do |flight|
+          flight.aircraft.aircraft_cabins.map do |cabin|
+            FlightCabinSerializer.new(cabin).to_h
+          end
+        end
+      end
   end
 
 
-  attribute :aircraft_cabins do |flight|
-    flight.aircraft.aircraft_cabins.map do |cabin|
-      FlightCabinSerializer.new(cabin).to_h
+
+  nested :flight_seats do
+    attribute :unavailable_seats do |flight|
+      flight.seats.not_available.map do |seat|
+        {
+          seat_code: seat.seat_code,
+          cabin_code: seat.cabin_code,
+          availability_state: seat.availability_state
+        }
+      end
+
     end
+    attribute :components do
+      {}
+    end
+
   end
 
-  attribute :unavailable_seats do |flight|
-    flight.seats.not_available.map do |seat|
-      {
-        seat_code: seat.seat_code,
-        cabin_code: seat.cabin_code,
-        availability_state: seat.availability_state
-      }
-    end
-  end
 
 
 end

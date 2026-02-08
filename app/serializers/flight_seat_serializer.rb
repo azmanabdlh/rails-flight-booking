d046fe5d4bc6cedc39682c0455235c85a66bd81f
Azman <macbookpro@MacBook-Pro-13.local>
2026-02-09 08:10:29 +0900

    add flight.aircraft

--- app/serializers/flight_seat_serializer.rb
+++ app/serializers/flight_seat_serializer.rb
@@ -1,11 +1,20 @@
 class FlightSeatSerializer
   include Alba::Resource
 
-  attributes :flight_code,
-    :departure_airport,
-    :arrival_airport,
-    :departure_time,
-    :arrival_time
+
+  nested :flight do
+    attributes :flight_code,
+      :departure_airport,
+      :arrival_airport,
+      :departure_time,
+      :arrival_time
+
+
+    attribute :aircraft do |flight|
+      { model: flight.aircraft.model, variant: flight.aircraft.variant }
+    end
+  end
+
 
   attribute :aircraft_cabins do |flight|
     flight.aircraft.aircraft_cabins.map do |cabin|
@@ -15,7 +24,11 @@ class FlightSeatSerializer
 
   attribute :unavailable_seats do |flight|
     flight.seats.not_available.map do |seat|
-      { seat_code: seat.seat_code, cabin_code: seat.cabin_code, availability_state: seat.availability_state }
+      {
+        seat_code: seat.seat_code,
+        cabin_code: seat.cabin_code,
+        availability_state: seat.availability_state
+      }
     end
   end
 

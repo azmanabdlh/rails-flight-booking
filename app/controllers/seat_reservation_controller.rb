2884e046f5917f1d3ca5c8d524403c22a4e206dc
Azman <macbookpro@MacBook-Pro-13.local>
2026-02-12 13:16:00 +0900

    fix typo

--- app/controllers/seat_reservation_controller.rb
+++ app/controllers/seat_reservation_controller.rb
@@ -1,29 +1,27 @@
 class SeatReservationController < ApplicationController
   def call
     begin
-      req = params.permit(:flight_id, :cabin_code, :seat_code, :booking_id)
+      request = params.permit(:flight_id, :cabin_code, :seat_code, :booking_id)
 
       raise ActiveRecord::RecordNotFound unless seat_code_valid_for_cabin?(
-        req[:flight_id],
-        req[:cabin_code],
-        req[:seat_code]
+        request[:flight_id],
+        request[:cabin_code],
+        request[:seat_code]
       )
 
-      book = Booking.find(req[:booking_id])
-      raise Booking::NotPaid.new "You booking is not paid yet."  if book.not.paid?
+      raise Booking::NotPaid.new "You booking is not paid yet." unless book.paid? Booking.find(request[:booking_id])
 
-       Ticket.find(req[:ticket_id])
-        .start_confirm_seat!(
-          req[:flight_id],
-          req[:seat_code],
+       Ticket.find(request[:ticket_id]).start_confirm_seat!(
+          request[:flight_id],
+          request[:seat_code],
           user_id = 1
         )
 
-      render json: { message: "Seat #{req[:seat_code]} booked", success: true }, status: :created
+      render json: { message: "Seat #{request[:seat_code]} booked", success: true }, status: :created
     rescue ActiveRecord::RecordNotFound
       render json: { message: "Seat not found" }, status: :not_found
     rescue => e
-      render json: { message: e.message }, status: :bad_request
+      render json: { message: e.message }, status: :bad_requestuest
     end
   end
 

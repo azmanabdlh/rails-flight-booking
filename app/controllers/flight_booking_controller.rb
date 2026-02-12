2b67cf4a52187e37b21d90fb2aa690a611ff0ea7
Azman <macbookpro@MacBook-Pro-13.local>
2026-02-12 23:00:53 +0900

    fix

--- app/controllers/flight_booking_controller.rb
+++ app/controllers/flight_booking_controller.rb
@@ -1,11 +1,12 @@
 class FlightBookingController < ApplicationController
   def new
-    flight = Flight.find(params[:flight_id])
+    flight = Flight.find(
+      params[:flight_id]
+    )
     return render json: { message: "invalid flight id" }, status: :bad_request  unless flight.open_for_sale?
 
     Booking.start_booking(
       flight.id,
-      params[:flight_id],
       user_id = 1
     )
 
